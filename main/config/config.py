4c99d2f9aa7b573933b7178996b6e9dcfeac154a
Jaillet Loic <loic.jaillet@student-cs.fr>
2025-01-30 11:21:46 -0800

    date format

--- main/config/config.py
+++ main/config/config.py
@@ -25,7 +25,7 @@ import sys
 
 TRAINING_DATA_FILE = "data/dow_30_2009_2020.csv"
 
-now = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
+now = datetime.datetime.now().strftime('%Y-%m-%d %H;%M;%S')
 TRAINED_MODEL_DIR = f"trained_models/{now}"
 os.makedirs(TRAINED_MODEL_DIR)
 TURBULENCE_DATA = "data/dow30_turbulence_index.csv"
